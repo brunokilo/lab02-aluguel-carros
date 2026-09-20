d040a4be257f9f71238e13001582d5d2159b5af3
Jao <jvplorencini@gmail.com>
2026-09-20 19:02:04 -0300

    feat: adicionar classes Usuario, ClienteDTO e métodos de conversão em Cliente

--- src/alucar/src/main/java/com/alucar/alucar/model/Cliente.java
+++ src/alucar/src/main/java/com/alucar/alucar/model/Cliente.java
@@ -3,6 +3,8 @@ package com.alucar.alucar.model;
 import java.util.LinkedList;
 import java.util.List;
 
+import com.alucar.alucar.dto.ClienteDTO;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -19,20 +21,13 @@ import lombok.Setter;
 @Setter
 @Entity 
 @NoArgsConstructor 
-public class Cliente {
-    
-    @Id 
-    @GeneratedValue(strategy = GenerationType.IDENTITY) 
-    private Long id;
+public class Cliente extends Usuario{
 
     @Column(unique=true)
     private String rg;
     
     @Column(unique=true, nullable = false, length = 11)
     private String cpf;
-    
-    @Column(nullable = false)
-    private String nome;
     
     @Column(nullable = false)
     private String profissao;
@@ -44,5 +39,19 @@ public class Cliente {
     private List<Empregador> empregadores = new LinkedList<>();
     
     @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL, orphanRemoval = true)
+    
     private List<Pedido> pedidos = new LinkedList<>();
+
+    public void incorporarDTO(ClienteDTO cliente){
+        super.incorporarDTObase(cliente.usuario());
+        this.rg = cliente.rg();
+        this.cpf = cliente.cpf();
+        this.profissao = cliente.profissao();
+        this.endereco = cliente.endereco();
+        this.empregadores = cliente.empregadores();
+    }
+
+    public ClienteDTO criarDTO(){
+        return new ClienteDTO(super.criarDTObase(), rg, cpf, profissao, endereco, empregadores);
+    }
 }
