5ee99a4d2a5affa7f4707c1c87414ff972d4cbc3
Jao <jvplorencini@gmail.com>
2026-09-21 09:16:02 -0300

    feat: remover importação não utilizada de EnderecoDTO na classe Cliente

--- src/back/alucar/src/main/java/com/alucar/alucar/model/Cliente.java
+++ src/back/alucar/src/main/java/com/alucar/alucar/model/Cliente.java
@@ -4,7 +4,6 @@ import java.util.LinkedList;
 import java.util.List;
 
 import com.alucar.alucar.dto.ClienteDTO;
-import com.alucar.alucar.dto.EnderecoDTO;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
